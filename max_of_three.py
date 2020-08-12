42643e7c575692c02a44ed2cc0d0e8928dc01406
Stefan <sjovicevic@protonmail.ch>
2020-08-12 08:40:58 +0200

    Added few new things

---
+++ max_of_three.py
@@ -0,0 +1,6 @@
+def maxn(a,b,c):
+    pass
+
+ ls = [int(input(">>> ")) for item in range(3)]
+ls.sort()
+print(ls[len(ls) - 1])
---
+++ tic_tac_2.py
@@ -0,0 +1,14 @@
+# checking whether there is a winner, 0 - empty field, 1 - player1, 2 - player2
+
+def winner(ls):
+    for i in ls:
+        for j in ls:
+            
+
+
+tictac = [  [1, 0, 2],
+            [1, 2, 0],
+            [2, 1, 0]]
+
+
+    
